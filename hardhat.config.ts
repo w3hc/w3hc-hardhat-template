bcc3e86076bb28dddfed803162fffd643e3003bf
julienbrg <julien@strat.cc>
2023-10-06 17:57:48 +0200

    apply prettier

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,38 +6,37 @@ import "./tasks/mint"
 import "./tasks/send"
 dotenv.config()
 
-const {
-  GOERLI_RPC_ENDPOINT_URL,
-  GOERLI_PRIVATE_KEY,
-  ETHERSCAN_API_KEY
-} = process.env
+const { GOERLI_RPC_ENDPOINT_URL, GOERLI_PRIVATE_KEY, ETHERSCAN_API_KEY } =
+    process.env
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
-  networks: {
-    'hardhat': {
-      chainId: 1337,
-      allowUnlimitedContractSize: true
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 1337,
+            allowUnlimitedContractSize: true
+        },
+        goerli: {
+            url:
+                GOERLI_RPC_ENDPOINT_URL || "https://goerli.gateway.tenderly.co",
+            accounts:
+                GOERLI_PRIVATE_KEY !== undefined ? [GOERLI_PRIVATE_KEY] : []
+        }
     },
-    'goerli': {
-      url: GOERLI_RPC_ENDPOINT_URL || "https://goerli.gateway.tenderly.co",
-      accounts: GOERLI_PRIVATE_KEY !== undefined ? [GOERLI_PRIVATE_KEY] : [],
-    }
-  }, 
-  solidity: {
-    version: "0.8.19",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  etherscan: {
-    apiKey: {
-      goerli: ETHERSCAN_API_KEY || ""
+    solidity: {
+        version: "0.8.19",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
     },
-  },
-};
+    etherscan: {
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY || ""
+        }
+    }
+}
 
 export default config
--- tasks/send.ts
+++ tasks/send.ts
@@ -1,17 +1,26 @@
-import { task } from "hardhat/config";
-var msg = (require("cli-color")).xterm(39).bgXterm(128)
-import * as store from '../store.json'
+import { task } from "hardhat/config"
+var msg = require("cli-color").xterm(39).bgXterm(128)
+import * as store from "../store.json"
 
 task("send", "Send a given amount of tokens to a given address")
-.addParam("wallet").addParam("amount").setAction(
-
-    async (args) => {
+    .addParam("wallet")
+    .addParam("amount")
+    .setAction(async args => {
         const [signer] = await ethers.getSigners()
-        const Basic = await ethers.getContractFactory('Basic')
+        const Basic = await ethers.getContractFactory("Basic")
         const addr = store.contractAddress
         const erc20 = new ethers.Contract(addr, Basic.interface, signer)
-        const mint = await erc20.transfer(args.wallet, await ethers.parseEther(args.amount))
+        const mint = await erc20.transfer(
+            args.wallet,
+            await ethers.parseEther(args.amount)
+        )
         const hash = mint.hash
-        console.log("\nSent", msg(args.amount), "to", args.wallet , "\n\nTx hash:", msg(hash))
-    }
-);
+        console.log(
+            "\nSent",
+            msg(args.amount),
+            "to",
+            args.wallet,
+            "\n\nTx hash:",
+            msg(hash)
+        )
+    })
