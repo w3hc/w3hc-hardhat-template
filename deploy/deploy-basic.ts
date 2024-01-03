7cb803495347974e47eb10be1235e6bddc741f5b
julienbrg <julien@strat.cc>
2024-01-03 22:31:00 +0100

    clean up

--- deploy/deploy-basic.ts
+++ deploy/deploy-basic.ts
@@ -1,9 +1,6 @@
 import "@nomiclabs/hardhat-ethers"
 
-//your address here...
 const initialMint = ethers.parseEther("10000")
-
-//to deploy, run yarn hardhat deploy --network hardhat
 
 export default async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -18,29 +18,29 @@ const {
 const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     namedAccounts: {
-      deployer: 0
+        deployer: 0
     },
     networks: {
-      hardhat: {
-          chainId: 1337,
-          allowUnlimitedContractSize: true
-      },
-      goerli: {
-          url:
-              GOERLI_RPC_ENDPOINT_URL || "https://goerli.gateway.tenderly.co",
-          accounts:
-              GOERLI_PRIVATE_KEY !== undefined ? [GOERLI_PRIVATE_KEY] : []
-      },
-      "arthera-testnet": {
-          url:
-              ARTHERA_TESTNET_RPC_ENDPOINT_URL ||
-              "https://rpc-test.arthera.net",
-          accounts:
-              ARTHERA_TESTNET_PRIVATE_KEY !== undefined
-                  ? [ARTHERA_TESTNET_PRIVATE_KEY]
-                  : []
-      }
-  },
+        hardhat: {
+            chainId: 1337,
+            allowUnlimitedContractSize: true
+        },
+        goerli: {
+            url:
+                GOERLI_RPC_ENDPOINT_URL || "https://goerli.gateway.tenderly.co",
+            accounts:
+                GOERLI_PRIVATE_KEY !== undefined ? [GOERLI_PRIVATE_KEY] : []
+        },
+        "arthera-testnet": {
+            url:
+                ARTHERA_TESTNET_RPC_ENDPOINT_URL ||
+                "https://rpc-test.arthera.net",
+            accounts:
+                ARTHERA_TESTNET_PRIVATE_KEY !== undefined
+                    ? [ARTHERA_TESTNET_PRIVATE_KEY]
+                    : []
+        }
+    },
     solidity: {
         version: "0.8.19",
         settings: {
@@ -49,7 +49,7 @@ const config: HardhatUserConfig = {
                 runs: 200
             }
         }
-    },
+    }
     // etherscan: {
     //     apiKey: {
     //         goerli: ETHERSCAN_API_KEY || ""
