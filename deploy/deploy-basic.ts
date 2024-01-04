2e3f6b48335b8011b83edb07b23d97d577567571
julienbrg <julien@strat.cc>
2024-01-04 12:53:47 +0100

    rm etherscan verification for arthera

--- deploy/deploy-basic.ts
+++ deploy/deploy-basic.ts
@@ -21,27 +21,33 @@ export default async ({ getNamedAccounts, deployments }: any) => {
     console.log("hre.network.name:", hre.network.name)
 
     switch (hre.network.name) {
-        // TODO: fix verification for Arthera Testnet using Sourcify
         case "arthera-testnet":
             console.log(
                 "Basic ERC-20 token contract deployed:",
                 msg(basic.receipt.contractAddress)
             )
+
             try {
-                console.log("\nEtherscan verification in progress...")
+                // Please use `pnpm sourcify:arthera` after the deployment instead.
+
+                // console.log("\nEtherscan verification in progress...")
+                // console.log(
+                //     "\nWaiting for 6 block confirmations (you can skip this part)"
+                // )
+                // await basic.deploymentTransaction()?.wait(6)
+                // await hre.run("verify:verify", {
+                //     network: network.name,
+                //     address: basic.receipt.contractAddress,
+                //     constructorArguments: [initialMint]
+                // })
+
                 console.log(
-                    "\nWaiting for 6 block confirmations (you can skip this part)"
+                    "Please use `pnpm sourcify:arthera` to verify your contract."
                 )
-                await basic.deploymentTransaction()?.wait(6)
-                await hre.run("verify:verify", {
-                    network: network.name,
-                    address: basic.receipt.contractAddress,
-                    constructorArguments: [initialMint]
-                })
-                console.log("Etherscan verification done. ✅")
             } catch (error) {
                 console.error(error)
             }
+
             break
         case "sepolia":
             try {
